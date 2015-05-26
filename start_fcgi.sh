4b56483126a8b49fc4a644e0be75965ed43c011c
Jason McCarver <slam@parasite.cc>
2015-05-26 14:55:56 -0500

    Updated start/stop fcgi scripts

--- start_fcgi.sh
+++ start_fcgi.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-script/dynip_fastcgi.pl -l 127.0.0.1:9001 -n 2
+
+dir=$HOME/app/DynIP
+nohup $dir/app/script/dynip_fastcgi.pl -l 127.0.0.1:9001 -n 2 > $dir/log/fastcgi.log 2>&1 &
---
+++ stop_fcgi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pkill -f "perl-fcgi-pm \[DynIP\]"
